26f864b4c2e915d70454ac9e2a66464bc491d333
Brandon Fulco <bfulco7@gmail.com>
2018-01-08 12:55:38 -0500

    Refactor Unit tests for extensibility

--- app/src/test/java/DependencyGraphTest.java
+++ app/src/test/java/DependencyGraphTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -5,63 +6,68 @@ import java.util.Collections;
 
 import static org.junit.Assert.assertEquals;
 
-public class DependencyGraphTest {
+public abstract class DependencyGraphTest {
+
+    protected DependencyGraph dependencyGraph;
+
+    public static class SimpleDependencyGraphTest extends DependencyGraphTest{
+
+        @Before
+        public void setup() {
+            dependencyGraph = new SimpleDependencyGraph();
+        }
+    }
 
     @Test
     public void test_basic() {
-        DependencyGraph graph = new SimpleDependencyGraph();
-        graph.addDirect("A", Arrays.asList("B", "C"));
-        graph.addDirect("B", Arrays.asList("C", "E"));
-        graph.addDirect("C", Arrays.asList("G"));
-        graph.addDirect("D", Arrays.asList("A", "F"));
-        graph.addDirect("E", Arrays.asList("F"));
-        graph.addDirect("F", Arrays.asList("H"));
-        graph.addDirect("G", Collections.emptyList());
-        graph.addDirect("H", Collections.emptyList());
+        dependencyGraph.addDirect("A", Arrays.asList("B", "C"));
+        dependencyGraph.addDirect("B", Arrays.asList("C", "E"));
+        dependencyGraph.addDirect("C", Arrays.asList("G"));
+        dependencyGraph.addDirect("D", Arrays.asList("A", "F"));
+        dependencyGraph.addDirect("E", Arrays.asList("F"));
+        dependencyGraph.addDirect("F", Arrays.asList("H"));
+        dependencyGraph.addDirect("G", Collections.emptyList());
+        dependencyGraph.addDirect("H", Collections.emptyList());
 
-        assertEquals(Arrays.asList("B", "C", "E", "F", "G", "H"), graph.dependenciesFor("A"));
-        assertEquals(Arrays.asList("C", "E", "F", "G", "H"), graph.dependenciesFor("B"));
-        assertEquals(Arrays.asList("G"), graph.dependenciesFor("C"));
-        assertEquals(Arrays.asList("A", "B", "C", "E", "F", "G", "H"), graph.dependenciesFor("D"));
-        assertEquals(Arrays.asList("F", "H"), graph.dependenciesFor("E"));
-        assertEquals(Arrays.asList("H"), graph.dependenciesFor("F"));
+        assertEquals(Arrays.asList("B", "C", "E", "F", "G", "H"), dependencyGraph.dependenciesFor("A"));
+        assertEquals(Arrays.asList("C", "E", "F", "G", "H"), dependencyGraph.dependenciesFor("B"));
+        assertEquals(Arrays.asList("G"), dependencyGraph.dependenciesFor("C"));
+        assertEquals(Arrays.asList("A", "B", "C", "E", "F", "G", "H"), dependencyGraph.dependenciesFor("D"));
+        assertEquals(Arrays.asList("F", "H"), dependencyGraph.dependenciesFor("E"));
+        assertEquals(Arrays.asList("H"), dependencyGraph.dependenciesFor("F"));
     }
 
     @Test
     public void addEmptyGraph_hasEmtpyDependencies() {
-        DependencyGraph graph = new SimpleDependencyGraph();
-        graph.addDirect("A", Collections.emptyList());
+        dependencyGraph.addDirect("A", Collections.emptyList());
 
-        assertEquals(Collections.emptyList(), graph.dependenciesFor("A"));
+        assertEquals(Collections.emptyList(), dependencyGraph.dependenciesFor("A"));
     }
 
     @Test
     public void addFlatGraph_hasCorrectDependencies() {
-        DependencyGraph graph = new SimpleDependencyGraph();
-        graph.addDirect("A", Arrays.asList("B"));
-        graph.addDirect("C", Arrays.asList("D"));
+        dependencyGraph.addDirect("A", Arrays.asList("B"));
+        dependencyGraph.addDirect("C", Arrays.asList("D"));
 
-        assertEquals(Arrays.asList("B"), graph.dependenciesFor("A"));
-        assertEquals(Arrays.asList("D"), graph.dependenciesFor("C"));
+        assertEquals(Arrays.asList("B"), dependencyGraph.dependenciesFor("A"));
+        assertEquals(Arrays.asList("D"), dependencyGraph.dependenciesFor("C"));
     }
 
     @Test
     public void addSimpleGraph_hasSimpleDependencies() {
-        DependencyGraph graph = new SimpleDependencyGraph();
-        graph.addDirect("A", Arrays.asList("B"));
-        graph.addDirect("B", Arrays.asList("C"));
+        dependencyGraph.addDirect("A", Arrays.asList("B"));
+        dependencyGraph.addDirect("B", Arrays.asList("C"));
 
-        assertEquals(Arrays.asList("B", "C"), graph.dependenciesFor("A"));
-        assertEquals(Arrays.asList("C"), graph.dependenciesFor("B"));
+        assertEquals(Arrays.asList("B", "C"), dependencyGraph.dependenciesFor("A"));
+        assertEquals(Arrays.asList("C"), dependencyGraph.dependenciesFor("B"));
     }
 
     @Test
     public void addCyclicGraph_hasCyclicDependencies() {
-        DependencyGraph graph = new SimpleDependencyGraph();
-        graph.addDirect("A", Arrays.asList("B"));
-        graph.addDirect("B", Arrays.asList("A"));
+        dependencyGraph.addDirect("A", Arrays.asList("B"));
+        dependencyGraph.addDirect("B", Arrays.asList("A"));
 
-        assertEquals(Arrays.asList("A", "B"), graph.dependenciesFor("A"));
-        assertEquals(Arrays.asList("A", "B"), graph.dependenciesFor("B"));
+        assertEquals(Arrays.asList("A", "B"), dependencyGraph.dependenciesFor("A"));
+        assertEquals(Arrays.asList("A", "B"), dependencyGraph.dependenciesFor("B"));
     }
 }
